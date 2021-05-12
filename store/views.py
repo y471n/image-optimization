043844966fe8412cc06729e03794453981310823
Yatin <iamyatin@gmail.com>
2021-05-12 09:06:00 +0530

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+def index(request):
+    return HttpResponse('Hello')
+
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html')
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html')
